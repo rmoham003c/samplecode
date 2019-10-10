18db0d46fb79930e9f571afe921cd48fae60f27f
Riaz M <riaz4aix@gmail.com>
2019-10-10 02:07:47 +0000

    Modified helloworld.java and added goodbye.java

---
+++ goodbye.java
@@ -0,0 +1,11 @@
+/* This is a simple Java program. 
+   FileName : "goodbye.java". */
+class GoodBye 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Good Bye" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Good Bye"); 
+    } 
+} 
