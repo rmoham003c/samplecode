189e6bac2657f803e308866752832bfb8fb65393
Riaz M <riaz4aix@gmail.com>
2019-10-10 02:11:47 +0000

    Added new print stmt to helloworld

--- helloworld.java
+++ helloworld.java
@@ -7,5 +7,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello There, World"); 
+        
+        System.out.println("Welcome to GIT World");
     } 
 } 
